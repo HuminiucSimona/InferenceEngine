ea916e1ca7ca177c12994c42f825a5e676218103
Simona Huminiuc <simona9302@gmail.com>
2024-12-18 18:45:27 +0200

    add variable class

---
+++ Variable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InferenceEngine
+{
+    /// <summary>
+    /// reprezinta o variabila in motorul de inferenta predicativa
+    /// clasa este folosita pentru a stoca numele variabilelor in expresiile logice
+    /// </summary>  
+    public class Variable
+    {
+        public string Name { get; set; }
+
+        public Variable(string name)
+        {
+            this.Name = name;
+        }
+        override public string ToString()
+        {
+            return Name;
+        }
+    }
+}
